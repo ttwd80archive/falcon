216497e022c76bd0613d09ea73019a93700f5f49
Titi Wangsa bin Damhore <titi.wangsa.damhore@tabuk-tech.com>
2013-02-17 17:06:03 +0800

    mail sender

---
+++ src/main/java/com/twistlet/falcon/model/service/MailSenderServiceImpl.java
@@ -0,0 +1,42 @@
+package com.twistlet.falcon.model.service;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailSenderServiceImpl implements MailSenderService {
+
+	private final String senderName;
+	private final String senderAddress;
+	private final String subject;
+	private final JavaMailSender javaMailSender;
+
+	@Autowired
+	public MailSenderServiceImpl(
+			@Value("${smtp.name}") final String senderName,
+			@Value("smtp.address") final String senderAddress,
+			@Value("smtp.subject") final String subject,
+			final JavaMailSender javaMailSender) {
+		this.senderName = senderName;
+		this.senderAddress = senderAddress;
+		this.subject = subject;
+		this.javaMailSender = javaMailSender;
+	}
+
+	@Override
+	public void send(final String sendTo, final String message) {
+		final SimpleMailMessage mailMessage = new SimpleMailMessage();
+		final String from = "\"" + senderName + "\" " + senderAddress;
+		mailMessage.setFrom(from);
+		mailMessage.setTo(sendTo);
+		mailMessage.setSentDate(new Date());
+		mailMessage.setSubject(subject);
+		mailMessage.setText(message);
+		javaMailSender.send(mailMessage);
+	}
+}
