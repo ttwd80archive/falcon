7e9a106e7382586591a01034f728ed6449bc0900
Titi Wangsa bin Damhore <titi.wangsa.damhore@tabuk-tech.com>
2013-02-17 12:21:45 +0800

    removed "Dump completed on" as we are using git

--- src/test/resources/db/database-dump.sh
+++ src/test/resources/db/database-dump.sh
@@ -4,7 +4,7 @@ db_user=falcon
 db_password=phefrebre796ayuf
 db_name=falcon
 output_path=.
-mysqldump -d -h $db_host -u $db_user -p${db_password} ${db_name} > ${output_path}/${db_name}-structure.sql
+mysqldump -d -h $db_host -u $db_user -p${db_password} ${db_name} | grep -v "Dump completed on" > ${output_path}/${db_name}-structure.sql
 echo Structure dump. Done.
-mysqldump -u $db_user -p${db_password} --no-create-db --no-create-info --skip-extended-insert --complete-insert ${db_name} > ${output_path}/${db_name}-data.sql
+mysqldump -u $db_user -p${db_password} --no-create-db --no-create-info --skip-extended-insert --complete-insert ${db_name} | grep -v "Dump completed on" > ${output_path}/${db_name}-data.sql
 echo Data dump. Done.
