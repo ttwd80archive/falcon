f4361023f9d19a1064da132d5789f9630f163209
Titi Wangsa bin Damhore <titi.wangsa.damhore@tabuk-tech.com>
2013-02-18 10:13:43 +0800

    main controller to redirect to proper page

---
+++ src/main/java/com/twistlet/falcon/controller/MainController.java
@@ -0,0 +1,50 @@
+package com.twistlet.falcon.controller;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.twistlet.falcon.security.service.SecurityContextService;
+
+@Controller
+public class MainController {
+
+	private final SecurityContextService securityContextService;
+	private final Map<String, String> map;
+
+	@Autowired
+	public MainController(final SecurityContextService securityContextService,
+			@Qualifier("welcomePageMap") final Map<String, String> map) {
+		this.securityContextService = securityContextService;
+		this.map = Collections.unmodifiableMap(map);
+	}
+
+	@RequestMapping("/main")
+	public ModelAndView main() {
+		final List<? extends GrantedAuthority> list = new ArrayList<>(
+				securityContextService.getAuthorities());
+		final List<String> grantedAuthorityList = new ArrayList<>();
+		for (final GrantedAuthority grantedAuthority : list) {
+			grantedAuthorityList.add(grantedAuthority.toString());
+		}
+		final Set<String> roleSet = map.keySet();
+		for (final String role : roleSet) {
+			if (grantedAuthorityList.contains(role)) {
+				final String view = map.get(role);
+				final ModelAndView mav = new ModelAndView(view);
+				return mav;
+			}
+		}
+		return null;
+
+	}
+}
