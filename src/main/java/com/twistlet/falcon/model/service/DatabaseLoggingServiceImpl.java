2c60ab6b8c597ee1afdac5645b88e6713ac0f304
Titi Wangsa bin Damhore <titi.wangsa.damhore@tabuk-tech.com>
2013-02-18 20:26:46 +0800

    logged who subit the request to send the notification

--- src/main/java/com/twistlet/falcon/model/service/DatabaseLoggingServiceImpl.java
+++ src/main/java/com/twistlet/falcon/model/service/DatabaseLoggingServiceImpl.java
@@ -3,22 +3,27 @@ package com.twistlet.falcon.model.service;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.twistlet.falcon.model.entity.FalconMessageLog;
 import com.twistlet.falcon.model.repository.FalconMessageLogRepository;
+import com.twistlet.falcon.security.service.SecurityContextService;
 
 @Service
 public class DatabaseLoggingServiceImpl implements DatabaseLoggingService {
 
 	private final FalconMessageLogRepository falconMessageLogRepository;
+	private final SecurityContextService securityContextService;
 
 	@Autowired
 	public DatabaseLoggingServiceImpl(
-			final FalconMessageLogRepository falconMessageLogRepository) {
+			final FalconMessageLogRepository falconMessageLogRepository,
+			final SecurityContextService securityContextService) {
 		this.falconMessageLogRepository = falconMessageLogRepository;
+		this.securityContextService = securityContextService;
 	}
 
 	@Override
@@ -31,12 +36,15 @@ public class DatabaseLoggingServiceImpl implements DatabaseLoggingService {
 
 	private void logMessageSent(final String address, final String message,
 			final String errorMessage, final String messageType) {
+		final Authentication auth = securityContextService.getAuthentication();
+		final String name = auth.getName();
 		final FalconMessageLog item = new FalconMessageLog();
 		item.setDestination(address);
 		item.setErrorMessage(errorMessage);
 		item.setMessage(message);
 		item.setMessageType(messageType);
 		item.setSentTime(new Date());
+		item.setSender(name);
 		falconMessageLogRepository.save(item);
 	}
 
