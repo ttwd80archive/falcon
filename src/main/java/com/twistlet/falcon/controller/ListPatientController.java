4801a74d5d1348cf8b81f3fa50b14285d92b0d15
Titi Wangsa bin Damhore <titi.wangsa.damhore@tabuk-tech.com>
2013-02-18 16:58:59 +0800

    ajax controller for displaying a list of patients

--- src/main/java/com/twistlet/falcon/controller/ListPatientController.java
+++ src/main/java/com/twistlet/falcon/controller/ListPatientController.java
@@ -1,13 +1,43 @@
 package com.twistlet.falcon.controller;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.twistlet.falcon.model.entity.FalconUser;
+import com.twistlet.falcon.model.service.StaffService;
 
 @Controller
 public class ListPatientController {
 
+	private final StaffService staffService;
+
+	@Autowired
+	public ListPatientController(final StaffService staffService) {
+		this.staffService = staffService;
+	}
+
 	@RequestMapping("/list-patient")
-	public String[] listPatient() {
-		return new String[] {};
+	@ResponseBody
+	public List<Map<String, String>> listPatient(
+			@RequestParam("term") final String partialName) {
+		final List<FalconUser> users = staffService.listPatients(partialName);
+		final List<Map<String, String>> list = new ArrayList<>();
+		for (final FalconUser falconUser : users) {
+			final Map<String, String> map = new LinkedHashMap<>();
+			map.put("name", falconUser.getName());
+			map.put("phone", StringUtils.trimToEmpty(falconUser.getPhone()));
+			map.put("mail", StringUtils.trimToEmpty(falconUser.getEmail()));
+			list.add(map);
+		}
+		return list;
 	}
 }
