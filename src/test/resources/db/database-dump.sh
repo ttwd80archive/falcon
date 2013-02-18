a445b7f04206d096e957d1a5be0ba2d547d942ea
Titi Wangsa bin Damhore <titi.wangsa.damhore@tabuk-tech.com>
2013-02-19 05:38:52 +0800

    output dump to script directory

--- src/test/resources/db/database-dump.sh
+++ src/test/resources/db/database-dump.sh
@@ -3,7 +3,8 @@ db_host=localhost
 db_user=falcon
 db_password=phefrebre796ayuf
 db_name=falcon
-output_path=.
+output_path="`dirname \"$0\"`"
+
 mysqldump -d -h $db_host -u $db_user -p${db_password} ${db_name} | grep -v "Dump completed on" > ${output_path}/${db_name}-structure.sql
 echo Structure dump. Done.
 mysqldump -u $db_user -p${db_password} --no-create-db --no-create-info --skip-extended-insert --complete-insert ${db_name} | grep -v "Dump completed on" > ${output_path}/${db_name}-data.sql
