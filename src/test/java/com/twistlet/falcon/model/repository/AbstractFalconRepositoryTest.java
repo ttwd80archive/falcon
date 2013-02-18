9cf8b979e1c50a1f84ec0bc673a24e36309f19c1
Titi Wangsa bin Damhore <titi.wangsa.damhore@tabuk-tech.com>
2013-02-18 13:21:13 +0800

    base test class for all repository tests

---
+++ src/test/java/com/twistlet/falcon/model/repository/AbstractFalconRepositoryTest.java
@@ -0,0 +1,8 @@
+package com.twistlet.falcon.model.repository;
+
+import org.springframework.test.context.ContextConfiguration;
+
+@ContextConfiguration({ "classpath:" })
+public abstract class AbstractFalconRepositoryTest {
+
+}
