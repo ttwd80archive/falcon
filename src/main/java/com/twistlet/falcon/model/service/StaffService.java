9634e2a073299f2f32aa6d57c21aa50ad45d6d87
Titi Wangsa bin Damhore <titi.wangsa.damhore@tabuk-tech.com>
2013-02-18 13:21:27 +0800

    staff related services

---
+++ src/main/java/com/twistlet/falcon/model/service/StaffService.java
@@ -0,0 +1,9 @@
+package com.twistlet.falcon.model.service;
+
+import java.util.List;
+
+import com.twistlet.falcon.model.entity.FalconUser;
+
+public interface StaffService {
+	List<FalconUser> listPatients();
+}
---
+++ src/main/java/com/twistlet/falcon/model/service/StaffServiceImpl.java
@@ -0,0 +1,28 @@
+package com.twistlet.falcon.model.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.twistlet.falcon.model.entity.FalconUser;
+import com.twistlet.falcon.model.repository.FalconUserRepository;
+
+@Service
+public class StaffServiceImpl implements StaffService {
+
+	private final FalconUserRepository falconUserRepository;
+
+	@Autowired
+	public StaffServiceImpl(final FalconUserRepository falconUserRepository) {
+		this.falconUserRepository = falconUserRepository;
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<FalconUser> listPatients() {
+		return falconUserRepository.findByRolename("ROLE_PATRON");
+	}
+
+}
