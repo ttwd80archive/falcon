ee24381fc2aeeff94f12b6700e9c726b83802126
Titi Wangsa bin Damhore <titi.wangsa.damhore@tabuk-tech.com>
2013-02-18 20:26:24 +0800

    ajax send data

--- src/main/webapp/js/send_notification.js
+++ src/main/webapp/js/send_notification.js
@@ -1,6 +1,10 @@
 $(function() {
 	function handle_item(item) {
 		$("#recepient").val(item.name);
+		$("#send-name").val(item.name);
+		$("#send-phone").val(item.phone);
+		$("#send-mail").val(item.mail);
+
 	}
 
 	function do_nothing() {
@@ -16,4 +20,26 @@ $(function() {
 	}).data("ui-autocomplete")._renderItem = function(ul, item) {
 		return $("<li>").append("<a>" + item.name + "</a>").appendTo(ul);
 	};
+
+	$("#button-submit").click(function() {
+		var isMailChecked = $('#notification-mail').is(":checked");
+		var isSmsChecked = $('#notification-sms').is(":checked");
+		if ($("#send-name").val() == "") {
+			alert("please choose a patient.");
+			return false;
+		}
+		if (isMailChecked == false && isSmsChecked == false) {
+			alert("No notification method selected.");
+			return false;
+		}
+		if (isMailChecked == false) {
+			$("#send-mail").val("");
+		}
+		if (isSmsChecked == false) {
+			$("#send-phone").val("");
+		}
+		$("#send-message").val($("#message-content").val());
+		return false;
+
+	});
 });
