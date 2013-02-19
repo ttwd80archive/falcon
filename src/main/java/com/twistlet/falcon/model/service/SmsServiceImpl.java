88f642d279822b5a49b9ea7acb3d7e5a154f6be4
Titi Wangsa bin Damhore <titi.wangsa.damhore@tabuk-tech.com>
2013-02-19 15:09:44 +0800

    tranposed nessage and sms number

--- src/main/java/com/twistlet/falcon/model/service/SmsServiceImpl.java
+++ src/main/java/com/twistlet/falcon/model/service/SmsServiceImpl.java
@@ -4,6 +4,8 @@ import java.net.URLEncoder;
 import java.text.MessageFormat;
 
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -16,6 +18,7 @@ public class SmsServiceImpl implements SmsService {
 
 	private final String smsGatewayLocation;
 	private final DatabaseLoggingService databaseLoggingService;
+	protected final Logger logger = LoggerFactory.getLogger(getClass());
 
 	@Autowired
 	public SmsServiceImpl(
@@ -33,7 +36,8 @@ public class SmsServiceImpl implements SmsService {
 			final String urlEncodedMessage = URLEncoder
 					.encode(message, "UTF-8");
 			final String location = MessageFormat.format(smsGatewayLocation,
-					urlEncodedMessage, sendTo);
+					sendTo, urlEncodedMessage);
+			logger.info("Invoking: {}", location);
 			final HtmlPage htmlPage = webClient.getPage(location);
 			final String content = htmlPage.asText();
 			if (!StringUtils.trimToEmpty(content).startsWith("1701:")) {
