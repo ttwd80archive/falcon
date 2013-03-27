f604199c35e91c6da961f0d708000b8f673e9fb1
helmy <helmy.ambotang@gmail.com>
2013-03-28 01:49:14 +0800

    Bean used to generate Json string

---
+++ src/main/java/com/twistlet/falcon/controller/bean/User.java
@@ -0,0 +1,27 @@
+package com.twistlet.falcon.controller.bean;
+
+public class User {
+	
+	String username;
+	
+	String name;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
