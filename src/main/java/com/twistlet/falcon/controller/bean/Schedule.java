eef22c2ee2f28cb0bd868ea97fa765f25ddbfca0
helmy <helmy.ambotang@gmail.com>
2013-04-03 00:46:09 +0800

    data structure for monthly schedule

---
+++ src/main/java/com/twistlet/falcon/controller/bean/Schedule.java
@@ -0,0 +1,27 @@
+package com.twistlet.falcon.controller.bean;
+
+public class Schedule {
+	
+	private int day;
+	
+	private int totalAppointment;
+
+	public int getDay() {
+		return day;
+	}
+
+	public void setDay(int day) {
+		this.day = day;
+	}
+
+	public int getTotalAppointment() {
+		return totalAppointment;
+	}
+
+	public void setTotalAppointment(int totalAppointment) {
+		this.totalAppointment = totalAppointment;
+	}
+	
+	
+
+}
