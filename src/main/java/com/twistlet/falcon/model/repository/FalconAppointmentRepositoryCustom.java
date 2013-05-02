f033c83b275006d8450521e7e381948a03cc9c49
helmy <helmy.ambotang@gmail.com>
2013-05-02 10:09:25 +0800

    change signiture

--- src/main/java/com/twistlet/falcon/model/repository/FalconAppointmentRepositoryCustom.java
+++ src/main/java/com/twistlet/falcon/model/repository/FalconAppointmentRepositoryCustom.java
@@ -4,14 +4,10 @@ import java.util.Date;
 import java.util.List;
 
 import com.twistlet.falcon.model.entity.FalconAppointment;
-import com.twistlet.falcon.model.entity.FalconLocation;
-import com.twistlet.falcon.model.entity.FalconPatron;
-import com.twistlet.falcon.model.entity.FalconService;
-import com.twistlet.falcon.model.entity.FalconUser;
 
 public interface FalconAppointmentRepositoryCustom {
 	
 	List<FalconAppointment> listFullByAppointmentDateBetween(Date start, Date end);
 
-	List<FalconAppointment> listAppointmentsByParam(FalconUser staff, FalconPatron patron, Date searchDate, FalconLocation location, FalconService service);
+	List<FalconAppointment> listAppointmentsByParam(Integer staffId, String patronId, Integer serviceId, Integer locationId, Date searchDate);
 }
