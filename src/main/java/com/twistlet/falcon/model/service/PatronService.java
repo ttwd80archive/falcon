e795fc83773ef6b79c12fa162afbdccbc5078c0d
titi wangsa <titi.wangsa.damhore@tabuk-tech.com>
2013-05-18 20:08:05 +0800

    cosmetic: organized imports

--- src/main/java/com/twistlet/falcon/model/service/PatronService.java
+++ src/main/java/com/twistlet/falcon/model/service/PatronService.java
@@ -4,33 +4,30 @@ import java.util.Date;
 import java.util.List;
 import java.util.Set;
 
-import org.springframework.transaction.annotation.Transactional;
-
 import com.twistlet.falcon.controller.bean.User;
 import com.twistlet.falcon.model.entity.FalconPatron;
-import com.twistlet.falcon.model.entity.FalconStaff;
 import com.twistlet.falcon.model.entity.FalconUser;
 
 public interface PatronService {
 
 	List<User> listRegisteredPatrons(FalconUser admin);
-	
+
 	Set<User> listAvailablePatrons(FalconUser admin, Date start, Date end);
-	
+
 	void savePatron(FalconPatron patron);
-	
+
 	FalconPatron findPatron(String patron, String admin);
-	
+
 	List<FalconPatron> listPatronByAdminNameLike(FalconUser admin, String name);
-	
+
 	List<FalconPatron> listPatronByAdminNricLike(FalconUser admin, String nric);
-	
+
 	List<FalconPatron> listPatronByAdminEmailLike(FalconUser admin, String nric);
-	
+
 	List<FalconPatron> listPatronByAdminMobileLike(FalconUser admin, String mobile);
 
 	List<FalconPatron> listPatronByAdminPatronLike(FalconUser admin, FalconUser patron);
-	
+
 	void deletePatron(FalconPatron patron);
 
 }
