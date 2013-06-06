93c54cb8e4a475032707d5273b89b8c913494f1b
titi wangsa <titi.wangsa.damhore@tabuk-tech.com>
2013-06-07 05:51:48 +0800

    added a "findByAdmin" method needed for send notification.

--- src/test/java/com/twistlet/falcon/model/repository/FalconUserRepositoryImplTest.java
+++ src/test/java/com/twistlet/falcon/model/repository/FalconUserRepositoryImplTest.java
@@ -1,6 +1,7 @@
 package com.twistlet.falcon.model.repository;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 import java.util.LinkedHashSet;
 import java.util.List;
@@ -14,6 +15,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.twistlet.falcon.model.entity.FalconPatron;
 import com.twistlet.falcon.model.entity.FalconRole;
 import com.twistlet.falcon.model.entity.FalconUser;
 import com.twistlet.falcon.model.entity.FalconUserRole;
@@ -45,8 +47,19 @@ public class FalconUserRepositoryImplTest extends AbstractFalconRepositoryTest {
 
 		// 1 user has role 3
 		entityManager.persist(new FalconUserRole(user2, role3));
+
+		entityManager.persist(createPatron(user1, user2));
+		entityManager.persist(createPatron(user2, user3));
+
 		entityManager.flush();
 		entityManager.clear();
+	}
+
+	private FalconPatron createPatron(FalconUser admin, FalconUser patron) {
+		FalconPatron falconPatron = new FalconPatron();
+		falconPatron.setFalconUserByAdmin(admin);
+		falconPatron.setFalconUserByPatron(patron);
+		return falconPatron;
 	}
 
 	private FalconUser createNewUser(final String username, final String email, final String nric, final String phone) {
@@ -60,7 +73,7 @@ public class FalconUserRepositoryImplTest extends AbstractFalconRepositoryTest {
 		falconUser.setValid(true);
 		return falconUser;
 	}
-	
+
 	private FalconRole createNewRole(final String rolename) {
 		final FalconRole falconRole = new FalconRole();
 		falconRole.setRoleName(rolename);
@@ -69,43 +82,37 @@ public class FalconUserRepositoryImplTest extends AbstractFalconRepositoryTest {
 
 	@Test
 	public void testFindByRolenameNoneFoundTotallyInvalid() {
-		final List<FalconUser> list = falconUserRepository
-				.findByRolenameAndNameLike("XXX", "");
+		final List<FalconUser> list = falconUserRepository.findByRolenameAndNameLike("XXX", "");
 		assertEquals(0, list.size());
 	}
 
 	@Test
 	public void testFindByRolenameNoneFoundButValidRole() {
-		final List<FalconUser> list = falconUserRepository
-				.findByRolenameAndNameLike("ROLE_LEVEL_2", "");
+		final List<FalconUser> list = falconUserRepository.findByRolenameAndNameLike("ROLE_LEVEL_2", "");
 		assertEquals(0, list.size());
 	}
 
 	@Test
 	public void testFindByRolenameNoneFoundButValidRoleValidName() {
-		final List<FalconUser> list = falconUserRepository
-				.findByRolenameAndNameLike("ROLE_LEVEL_2", "USER_1");
+		final List<FalconUser> list = falconUserRepository.findByRolenameAndNameLike("ROLE_LEVEL_2", "USER_1");
 		assertEquals(0, list.size());
 	}
 
 	@Test
 	public void testFindByRolenameShouldExistCount() {
-		final List<FalconUser> list = falconUserRepository
-				.findByRolenameAndNameLike("ROLE_LEVEL_3", "");
+		final List<FalconUser> list = falconUserRepository.findByRolenameAndNameLike("ROLE_LEVEL_3", "");
 		assertEquals(1, list.size());
 	}
 
 	@Test
 	public void testFindByRolenameShouldExist3Count() {
-		final List<FalconUser> list = falconUserRepository
-				.findByRolenameAndNameLike("ROLE_LEVEL_1", "");
+		final List<FalconUser> list = falconUserRepository.findByRolenameAndNameLike("ROLE_LEVEL_1", "");
 		assertEquals(3, list.size());
 	}
 
 	@Test
 	public void testFindByRolenameShouldExist3Values() {
-		final List<FalconUser> list = falconUserRepository
-				.findByRolenameAndNameLike("ROLE_LEVEL_1", "");
+		final List<FalconUser> list = falconUserRepository.findByRolenameAndNameLike("ROLE_LEVEL_1", "");
 		final Set<String> set = new LinkedHashSet<>();
 		for (final FalconUser falconUser : list) {
 			set.add(falconUser.getUsername());
@@ -116,16 +123,45 @@ public class FalconUserRepositoryImplTest extends AbstractFalconRepositoryTest {
 
 	@Test
 	public void testFindByRolenameShouldExistSingleCount() {
-		final List<FalconUser> list = falconUserRepository
-				.findByRolenameAndNameLike("ROLE_LEVEL_1", "2");
+		final List<FalconUser> list = falconUserRepository.findByRolenameAndNameLike("ROLE_LEVEL_1", "2");
 		assertEquals(1, list.size());
 	}
 
 	@Test
 	public void testFindByRolenameShouldExistSingleValue() {
-		final List<FalconUser> list = falconUserRepository
-				.findByRolenameAndNameLike("ROLE_LEVEL_1", "2");
+		final List<FalconUser> list = falconUserRepository.findByRolenameAndNameLike("ROLE_LEVEL_1", "2");
 		assertEquals("USER_2", list.get(0).getName());
+	}
+
+	@Test
+	public void testFindByAdminBad() {
+		assertEquals(0, falconUserRepository.findByAdmin("cartman").size());
+	}
+
+	@Test
+	public void testFindByAdmin1Size() {
+		List<FalconUser> list = falconUserRepository.findByAdmin("USER_1");
+		assertEquals(1, list.size());
+	}
+
+	@Test
+	public void testFindByAdmin1Content() {
+		List<FalconUser> list = falconUserRepository.findByAdmin("USER_1");
+		FalconUser patron = list.get(0);
+		assertEquals("email2@add.com", patron.getEmail());
+	}
+
+	@Test
+	public void testFindByAdmin2Size() {
+		List<FalconUser> list = falconUserRepository.findByAdmin("USER_2");
+		assertEquals(1, list.size());
+	}
+
+	@Test
+	public void testFindByAdmin2Content() {
+		List<FalconUser> list = falconUserRepository.findByAdmin("USER_2");
+		FalconUser patron = list.get(0);
+		assertEquals("email3@add.com", patron.getEmail());
 	}
 
 }
