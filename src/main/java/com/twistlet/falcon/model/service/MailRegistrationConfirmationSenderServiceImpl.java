3da70e3c40d0a1c4f40dafed86cefc7ebdf3f0ea
titi wangsa <titi.wangsa.damhore@tabuk-tech.com>
2013-07-01 02:40:39 +0800

    using dyanmic content

--- src/main/java/com/twistlet/falcon/model/service/MailRegistrationConfirmationSenderServiceImpl.java
+++ src/main/java/com/twistlet/falcon/model/service/MailRegistrationConfirmationSenderServiceImpl.java
@@ -1,12 +1,14 @@
 package com.twistlet.falcon.model.service;
 
+import java.io.IOException;
 import java.text.MessageFormat;
-import java.util.ArrayList;
-import java.util.List;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
@@ -14,39 +16,28 @@ import org.springframework.stereotype.Service;
 @Service
 public class MailRegistrationConfirmationSenderServiceImpl implements MailRegistrationConfirmationSenderService {
 
-	private MailSender mailSender;
+	private final MailSender mailSender;
+	private final String message;
 
 	@Autowired
-	public MailRegistrationConfirmationSenderServiceImpl(@Qualifier("signupMailSender") MailSender mailSender) {
+	public MailRegistrationConfirmationSenderServiceImpl(@Qualifier("signupMailSender") final MailSender mailSender,
+			@Value("${mail.content.signup}") final String messageLocation) {
 		this.mailSender = mailSender;
+		try {
+			message = StringUtils.join(FileUtils.readLines(new ClassPathResource(messageLocation).getFile()), "\n");
+		} catch (final IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	@Override
-	public void send(String fullName, String ic, String hp, String mail, String patronOf) {
-		SimpleMailMessage simpleMessage = new SimpleMailMessage();
-		List<String> lines = new ArrayList<>();
-		lines.add("Dear user,");
-		lines.add("");
-		lines.add("");
-		lines.add("Your email address your user id for future sign-ins.");
-		lines.add("");
-		lines.add("Full Name: {0}");
-		lines.add("IC NO: {1}");
-		lines.add("HP: {2}");
-		lines.add("Patron of {3}");
-		lines.add("");
-		lines.add("");
-		lines.add("Please log in & Schedule Away");
-		lines.add("");
-		lines.add("Warm Regards");
-		lines.add("Apris Solutions");
-		lines.add("");
+	public void send(final String fullName, final String ic, final String hp, final String mail, final String patronOf) {
+		final SimpleMailMessage simpleMessage = new SimpleMailMessage();
 		simpleMessage.setFrom("signup@butter-bun.com");
 		simpleMessage.setTo(MessageFormat.format("{0} <{1}>", new Object[] { fullName, mail }));
-		simpleMessage.setSubject("Butter-Bun Sign-Up");
-		String unformattedText = StringUtils.join(lines, "\n");
-		Object[] arguments = { fullName, ic, hp, patronOf };
-		String text = MessageFormat.format(unformattedText, arguments);
+		simpleMessage.setSubject("You have successfully signed up with Butter-Bun");
+		final Object[] arguments = { fullName, ic, hp, mail, patronOf };
+		final String text = MessageFormat.format(message, arguments);
 		simpleMessage.setText(text);
 		mailSender.send(simpleMessage);
 
