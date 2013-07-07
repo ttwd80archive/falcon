59bfe302961d196a57ff76d4a83f76b5a950abb7
titi wangsa <titi.wangsa.damhore@tabuk-tech.com>
2013-07-07 11:02:53 +0800

    fix quartz getting null pointer exception. quartz accpets injection by setter, and does not accept inection by constructor.

--- src/main/java/com/twistlet/falcon/model/service/ReminderServiceImpl.java
+++ src/main/java/com/twistlet/falcon/model/service/ReminderServiceImpl.java
@@ -30,7 +30,7 @@ import com.twistlet.falcon.model.entity.FalconUser;
 import com.twistlet.falcon.model.repository.FalconAppointmentRepository;
 import com.twistlet.falcon.model.repository.FalconUserRepository;
 
-@Service
+@Service("reminderService")
 public class ReminderServiceImpl implements ReminderService {
 
 	private final FalconAppointmentRepository falconAppointmentRepository;
