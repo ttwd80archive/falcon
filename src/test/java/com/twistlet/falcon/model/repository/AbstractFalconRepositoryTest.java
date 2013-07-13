8dde87153dcb87b92d17e4c1ca361134805cecfd
titi wangsa <titi.wangsa.damhore@tabuk-tech.com>
2013-07-14 06:14:57 +0800

    setting proper database settings for test

--- src/test/java/com/twistlet/falcon/model/repository/AbstractFalconRepositoryTest.java
+++ src/test/java/com/twistlet/falcon/model/repository/AbstractFalconRepositoryTest.java
@@ -3,11 +3,8 @@ package com.twistlet.falcon.model.repository;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 
-@ContextConfiguration({ "classpath:test-falcon-spring-config.xml",
-		"classpath:falcon-spring-data-source.xml",
-		"classpath:falcon-spring-entity-manager.xml",
-		"classpath:falcon-spring-data-jpa.xml" })
-public abstract class AbstractFalconRepositoryTest extends
-		AbstractTransactionalJUnit4SpringContextTests {
+@ContextConfiguration({ "classpath:test-falcon-spring-config.xml", "classpath:falcon-spring-data-source-test.xml",
+		"classpath:falcon-spring-entity-manager.xml", "classpath:falcon-spring-data-jpa.xml" })
+public abstract class AbstractFalconRepositoryTest extends AbstractTransactionalJUnit4SpringContextTests {
 
 }
