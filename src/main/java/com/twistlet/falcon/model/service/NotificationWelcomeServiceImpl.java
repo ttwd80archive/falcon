c3b22b21ae4174607c467268c8e7082be05981dc
titi wangsa <titi.wangsa.damhore@tabuk-tech.com>
2013-07-20 18:23:20 +0800

    using ascii

--- src/main/java/com/twistlet/falcon/model/service/NotificationWelcomeServiceImpl.java
+++ src/main/java/com/twistlet/falcon/model/service/NotificationWelcomeServiceImpl.java
@@ -2,6 +2,8 @@ package com.twistlet.falcon.model.service;
 
 import java.text.MessageFormat;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 
@@ -10,6 +12,7 @@ public class NotificationWelcomeServiceImpl implements NotificationWelcomeServic
 	private final SimpleMailMessage mailMessage;
 	private final MailSender mailSender;
 	private final String contentTemplate;
+	private final Logger logger = LoggerFactory.getLogger(getClass());
 
 	public NotificationWelcomeServiceImpl(final SimpleMailMessage mailMessage, final MailSender mailSender,
 			final String contentTemplate) {
@@ -23,7 +26,9 @@ public class NotificationWelcomeServiceImpl implements NotificationWelcomeServic
 			final String patronOf) {
 		final SimpleMailMessage mailMessage = new SimpleMailMessage(this.mailMessage);
 		final Object[] arguments = { fullName, ic, hp, mail, password, patronOf };
+		logger.info("send parameters [{}], [{}], [{}], [{}], [{}], [{}]", arguments);
 		final String content = MessageFormat.format(contentTemplate, arguments);
+		logger.info("content\n{}", content);
 		final String to = MessageFormat.format("\"{0}\" <{1}>", new Object[] { fullName, mail });
 		try {
 			mailMessage.setTo(to);
