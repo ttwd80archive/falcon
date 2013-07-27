b9736268658023f26b47d2394f174479b52f88bc
titi wangsa <titi.wangsa.damhore@tabuk-tech.com>
2013-07-27 18:15:43 +0800

    using html for mail sender

--- src/main/java/com/twistlet/falcon/model/service/NotificationServiceImpl.java
+++ src/main/java/com/twistlet/falcon/model/service/NotificationServiceImpl.java
@@ -2,28 +2,32 @@ package com.twistlet.falcon.model.service;
 
 import java.text.MessageFormat;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 
 public class NotificationServiceImpl implements NotificationService {
 
 	private final SimpleMailMessage mailMessage;
-	private final MailSender mailSender;
+	private final JavaMailSender javaMailSender;
 	private final String contentTemplate;
 	private final Logger logger = LoggerFactory.getLogger(getClass());
 
-	public NotificationServiceImpl(final SimpleMailMessage mailMessage, final MailSender mailSender, final String contentTemplate) {
+	public NotificationServiceImpl(final SimpleMailMessage mailMessage, final JavaMailSender javaMailSender,
+			final String contentTemplate) {
 		this.mailMessage = mailMessage;
-		this.mailSender = mailSender;
+		this.javaMailSender = javaMailSender;
 		this.contentTemplate = contentTemplate;
 	}
 
 	@Override
 	public void send(final String fullName, final String ic, final String hp, final String mail, final String password,
 			final String patronOf) {
-		final SimpleMailMessage mailMessage = new SimpleMailMessage(this.mailMessage);
 		final Object[] arguments = { fullName, ic, hp, mail, password, patronOf };
 		logger.info("send parameters [{}], [{}], [{}], [{}], [{}], [{}]", arguments);
 		final String contentOriginal = MessageFormat.format(contentTemplate, arguments);
@@ -32,10 +36,29 @@ public class NotificationServiceImpl implements NotificationService {
 		logger.info("content\n{}", content);
 		final String to = MessageFormat.format("\"{0}\" <{1}>", new Object[] { fullName, mail });
 		try {
-			mailMessage.setTo(to);
-			mailMessage.setText(content);
-			mailSender.send(mailMessage);
+			final MimeMessage message = javaMailSender.createMimeMessage();
+			final MimeMessageHelper helper = new MimeMessageHelper(message);
+			final String from = mailMessage.getFrom();
+			helper.setFrom(from);
+			if (mailMessage.getReplyTo() != null) {
+				helper.setReplyTo(mailMessage.getReplyTo());
+			}
+			if (mailMessage.getCc() != null) {
+				helper.setCc(mailMessage.getCc());
+			}
+			if (mailMessage.getBcc() != null) {
+				helper.setBcc(mailMessage.getBcc());
+			}
+			if (mailMessage.getSentDate() != null) {
+				helper.setSentDate(mailMessage.getSentDate());
+			}
+			helper.setTo(to);
+			helper.setSubject(mailMessage.getSubject());
+			helper.setText(content, true);
+			javaMailSender.send(message);
 		} catch (final RuntimeException e) {
+			e.printStackTrace();
+		} catch (final MessagingException e) {
 			e.printStackTrace();
 		}
 	}
--- src/test/java/com/twistlet/falcon/model/service/NotificationServiceImplTest.java
+++ src/test/java/com/twistlet/falcon/model/service/NotificationServiceImplTest.java
@@ -6,6 +6,11 @@ import static org.mockito.Mockito.*;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.Properties;
+
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.internet.MimeMessage;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.Before;
@@ -15,36 +20,65 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @RunWith(MockitoJUnitRunner.class)
 public class NotificationServiceImplTest {
 
 	NotificationService unit;
-
+	SimpleMailMessage mailMessage;
 	@Mock
-	MailSender mailSender;
+	JavaMailSender javaMailSender;
 
 	@Before
 	public void init() throws IOException {
 
-		final SimpleMailMessage mailMessage = new SimpleMailMessage();
+		mailMessage = new SimpleMailMessage();
+		mailMessage.setFrom("Butter-Bun Sign-Up <signup@butter-bun.com>");
+		mailMessage.setSubject("Welcome to Butter-Bun");
 		final Charset encoding = Charset.forName("US-ASCII");
 		final File contentFile = new ClassPathResource("com/twistlet/falcon/mail/welcome.txt").getFile();
 		final String contentTemplate = FileUtils.readFileToString(contentFile, encoding);
-		unit = new NotificationServiceImpl(mailMessage, mailSender, contentTemplate);
+		unit = new NotificationServiceImpl(mailMessage, javaMailSender, contentTemplate);
 	}
 
 	@Test
-	public void testSend() {
-		final ArgumentCaptor<SimpleMailMessage> argument = ArgumentCaptor.forClass(SimpleMailMessage.class);
-
+	public void testSend() throws IOException, MessagingException {
+		final MimeMessage mimeMessage = new MimeMessage((Session) null);
+		final ArgumentCaptor<MimeMessage> argument = ArgumentCaptor.forClass(MimeMessage.class);
+		when(javaMailSender.createMimeMessage()).thenReturn(mimeMessage);
 		unit.send("Name", "100", "200", "me@me.com", "abc123", "Shankar's Dental Clinic");
-		verify(mailSender).send(argument.capture());
-		final SimpleMailMessage mailMessage = argument.getValue();
-		assertTrue(!mailMessage.getText().contains("{0}"));
+		verify(javaMailSender).send(argument.capture());
+		final MimeMessage mailMessage = argument.getValue();
+		final String content = mailMessage.getContent().toString();
+		assertTrue(!content.contains("{0}"));
 
 	}
 
+	public static void main(final String[] args) throws IOException {
+		new NotificationServiceImplTest().execute();
+	}
+
+	public void execute() throws IOException {
+		mailMessage = new SimpleMailMessage();
+		mailMessage.setFrom("Butter-Bun Sign-Up <signup@butter-bun.com>");
+		mailMessage.setSubject("Welcome to Butter-Bun");
+		final Charset encoding = Charset.forName("US-ASCII");
+		final File contentFile = new ClassPathResource("com/twistlet/falcon/mail/welcome.txt").getFile();
+		final String contentTemplate = FileUtils.readFileToString(contentFile, encoding);
+		final JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+		javaMailSender.setHost("localhost");
+		javaMailSender.setPort(13306);
+		javaMailSender.setUsername("mailer@tabuk-tech.com");
+		javaMailSender.setPassword("8ym4X6CgOWx5hsQM7NCY");
+		final Properties properties = new Properties();
+		properties.setProperty("mail.smtp.auth", "true");
+		properties.setProperty("mail.smtp.starttls.enable", "true");
+		javaMailSender.setJavaMailProperties(properties);
+		final NotificationServiceImpl service = new NotificationServiceImpl(mailMessage, javaMailSender, contentTemplate);
+		service.send("Titi Wangsa", "100", "200", "tt@twistlet.com", "password", "Orga");
+
+	}
 }
